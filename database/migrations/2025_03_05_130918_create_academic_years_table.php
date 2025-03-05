3a83b676f86bdb9342aa9ced1c1393f6f5cc3ff2
RinnHehe <wafflemelons@gmail.com>
2025-03-05 23:31:49 +0800

    modified AcademicYear Model

--- database/migrations/2025_03_05_130918_create_academic_years_table.php
+++ database/migrations/2025_03_05_130918_create_academic_years_table.php
@@ -3,12 +3,10 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Enums\AcademicYearSemester;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('academic_years', function (Blueprint $table) {
@@ -20,12 +18,9 @@ return new class extends Migration
             $table->string('semester')->default(AcademicYearSemester::ODD->value);
             $table->boolean('is_active')->default(false);
             $table->timestamps();
-        });        
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('academic_years');
