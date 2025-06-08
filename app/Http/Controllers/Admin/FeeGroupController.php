3176eae5a76420f9f6ba81ffccb8031e2e4de274
RinnHehe <wafflemelons@gmail.com>
2025-06-09 05:01:29 +0800

    CRUD (FeeGroup)

--- app/Http/Controllers/Admin/FeeGroupController.php
+++ app/Http/Controllers/Admin/FeeGroupController.php
@@ -2,10 +2,143 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Enums\MessageType;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\FeeGroupRequest;
+use App\Http\Resources\Admin\FeeGroupResource;
+use App\Models\FeeGroup;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+use Throwable;
 
 class FeeGroupController extends Controller
 {
-    //
+    public function index(): Response
+    {
+        $feeGroups = FeeGroup::query()
+            ->select('id', 'group', 'amount', 'created_at')
+            ->filter(request()->only(['search']))
+            ->sorting(request()->only(['field', 'direction']))
+            ->paginate(request()->load ?? 10);
+
+        return Inertia::render('Admin/FeeGroups/Index', [
+            'page_settings' => [
+                'title' => 'Golongan UKT',
+                'subtitle' => 'Menampilkan semua golongan UKT yang tersedia pada platform ini',
+            ],
+            'feeGroups' => FeeGroupResource::collection($feeGroups)->additional([
+                'meta' => [
+                    'has_pages' => $feeGroups->hasPages(),
+                ],
+            ]),
+            'state' => [
+                'page' => request()->page ?? 1,
+                'search' => request()->search ?? '',
+                'load' => request()->load ?? 10,
+                'field' => request()->field ?? 'created_at',
+                'direction' => request()->direction ?? 'desc',
+            ],
+        ]);
+    }
+
+    public function create(): Response
+    {
+        return Inertia::render('Admin/FeeGroups/Create', [
+            'page_settings' => [
+                'title' => 'Tambah Golongan UKT',
+                'subtitle' => 'Menambahkan golongan UKT baru ke dalam platform',
+                'method' => 'POST',
+                'action' => route('admin.fee-groups.store'),
+            ],
+        ]);
+    }
+
+    public function store(FeeGroupRequest $request)
+    {
+        try {
+            $validated = $request->validated();
+
+            FeeGroup::create([
+                'group' => $validated['group'],
+                'amount' => $validated['amount'],
+            ]);
+
+            session()->flash('type', 'success');
+            session()->flash('message', MessageType::CREATED->message('Golongan UKT'));
+
+            return Inertia::location(route('admin.fee-groups.index'));
+        } catch (Throwable $e) {
+            return Inertia::render('Admin/FeeGroups/Create', [
+                'page_settings' => [
+                    'title' => 'Tambah Golongan UKT',
+                    'subtitle' => 'Menambahkan golongan UKT baru ke dalam platform',
+                    'method' => 'POST',
+                    'action' => route('admin.fee-groups.store'),
+                ],
+                'errors' => [
+                    'group' => $e->getMessage()
+                ]
+            ]);
+        }
+    }
+
+    public function edit(FeeGroup $feeGroup): Response
+    {
+        return Inertia::render('Admin/FeeGroups/Edit', [
+            'page_settings' => [
+                'title' => 'Edit Golongan UKT',
+                'subtitle' => 'Mengubah golongan UKT yang sudah ada pada platform',
+                'method' => 'PUT',
+                'action' => route('admin.fee-groups.update', $feeGroup),
+            ],
+            'feeGroup' => $feeGroup,
+        ]);
+    }
+
+    public function update(FeeGroupRequest $request, FeeGroup $feeGroup)
+    {
+        try {
+            $validated = $request->validated();
+
+            $feeGroup->update([
+                'group' => $validated['group'],
+                'amount' => $validated['amount'],
+            ]);
+
+            session()->flash('type', 'success');
+            session()->flash('message', MessageType::UPDATED->message('Golongan UKT'));
+
+            return Inertia::location(route('admin.fee-groups.index'));
+        } catch (Throwable $e) {
+            return Inertia::render('Admin/FeeGroups/Edit', [
+                'page_settings' => [
+                    'title' => 'Edit Golongan UKT',
+                    'subtitle' => 'Mengubah golongan UKT yang sudah ada pada platform',
+                    'method' => 'PUT',
+                    'action' => route('admin.fee-groups.update', $feeGroup),
+                ],
+                'feeGroup' => $feeGroup,
+                'errors' => [
+                    'group' => $e->getMessage()
+                ]
+            ]);
+        }
+    }
+
+    public function destroy(FeeGroup $feeGroup)
+    {
+        try {
+            $feeGroup->delete();
+
+            session()->flash('type', 'success');
+            session()->flash('message', MessageType::DELETED->message('Golongan UKT'));
+
+            return Inertia::location(route('admin.fee-groups.index'));
+        } catch (Throwable $e) {
+            return back()->withErrors([
+                'group' => $e->getMessage()
+            ]);
+        }
+    }
 }
---
+++ resources/js/Pages/Admin/FeeGroups/Edit.jsx
@@ -0,0 +1,112 @@
+import HeaderTitle from '@/Components/HeaderTitle';
+import { Button } from '@/Components/ui/button';
+import { Card, CardContent } from '@/Components/ui/card';
+import { Input } from '@/Components/ui/input';
+import { Label } from '@/Components/ui/label';
+import AppLayout from '@/Layouts/AppLayout';
+import { flashMessage } from '@/lib/utils';
+import { Link, useForm } from '@inertiajs/react';
+import { IconArrowLeft, IconCheck, IconCircleKey, IconDroplet, IconDroplets } from '@tabler/icons-react';
+import { toast } from 'sonner';
+
+export default function Edit(props) {
+    const { data, setData, post, processing, errors, reset } = useForm({
+        group: props.feeGroup.group ?? 1,
+        amount: props.feeGroup.amount ?? 0,
+        _method: props.page_settings.method,
+    });
+
+    const onHandleReset = () => {
+        reset();
+    };
+
+    const onHandleSubmit = (e) => {
+        e.preventDefault();
+
+        post(route('admin.fee-groups.update', props.feeGroup), {
+            group: Number(data.group),
+            amount: Number(data.amount)
+        }, {
+            preserveScroll: true,
+            preserveState: true,
+            onSuccess: (success) => {
+                const flash = flashMessage(success);
+                if (flash) {
+                    if (flash.type === 'success') {
+                        toast.success(flash.message);
+                    } else if (flash.type === 'error') {
+                        toast.error(flash.message);
+                    }
+                }
+            },
+            onError: (errors) => {
+                Object.keys(errors).forEach((key) => {
+                    toast.error(errors[key]);
+                });
+            },
+        });
+    };
+
+    return (
+        <div className="flex w-full flex-col pb-32">
+            <div className="mb-8 flex flex-col items-start justify-between gap-y-4 lg:flex-row lg:items-center">
+                <HeaderTitle
+                    title={props.page_settings.title}
+                    subtitle={props.page_settings.subtitle}
+                    icon={IconDroplets}
+                />
+                <Button variant="orange" size="xl" className="w-full lg:w-auto" asChild>
+                    <Link href={route('admin.fee-groups.index')}>
+                        <IconArrowLeft className="size-4" />
+                        Kembali
+                    </Link>
+                </Button>
+            </div>
+            <Card>
+                <CardContent className="p-6">
+                    <form onSubmit={onHandleSubmit}>
+                        <div className="grid grid-cols-1 gap-4 lg:grid-cols-4">
+                            <div className="col-span-full">
+                                <Label htmlFor="group">Golongan UKT</Label>
+                                <Input
+                                    type="text"
+                                    id="group"
+                                    name="group"
+                                    placeholder="Masukkan golongan UKT"
+                                    value={data.group}
+                                    onChange={(e) => setData(e.target.name, e.target.value)}
+                                    className={errors.group ? 'border-red-500 focus:border-red-500' : ''}
+                                />
+                                {errors.group && <div className="mt-2 text-sm text-red-600">{errors.group}</div>}
+                            </div>
+                            <div className="col-span-full">
+                                <Label htmlFor="amount">Jumlah</Label>
+                                <Input
+                                    type="number"
+                                    id="amount"
+                                    name="amount"
+                                    placeholder="Masukkan jumlah"
+                                    value={data.amount}
+                                    onChange={(e) => setData(e.target.name, e.target.value)}
+                                    className={errors.amount ? 'border-red-500 focus:border-red-500' : ''}
+                                />
+                                {errors.amount && <div className="mt-2 text-sm text-red-600">{errors.amount}</div>}
+                            </div>
+                        </div>
+                        <div className="mt-8 flex flex-col gap-2 lg:flex-row lg:justify-end">
+                            <Button type="button" variant="ghost" size="xl" onClick={onHandleReset}>
+                                Reset
+                            </Button>
+                            <Button type="submit" variant="blue" size="xl" disabled={processing}>
+                                <IconCheck className="mr-2 size-4" />
+                                Save
+                            </Button>
+                        </div>
+                    </form>
+                </CardContent>
+            </Card>
+        </div>
+    );
+}
+
+Edit.layout = (page) => <AppLayout children={page} title={page.props.page_settings.title} />;
