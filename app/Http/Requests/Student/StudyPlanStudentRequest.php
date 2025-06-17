e4a3f57f1220f73d18d7f61492d32a963721d98a
RinnHehe <wafflemelons@gmail.com>
2025-06-18 02:17:31 +0800

    Request Validation (KRS)

---
+++ app/Http/Requests/Student/StudyPlanStudentRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Student;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class StudyPlanStudentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::check() && Auth::user()->hasRole('Student');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'schedule_id' => ['required', 'array', 'exists:schedules,id'],
+        ];
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'schedule_id' => 'Jadwal',
+        ];
+    }
+}
