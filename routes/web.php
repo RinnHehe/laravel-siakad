c3aca05e7aa7127bd73247280865185230d3478a
RinnHehe <wafflemelons@gmail.com>
2025-07-01 00:13:17 +0800

    midtrans - Create Transaction

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -13,7 +14,7 @@ Route::get('/', function () {
     }
 });
 
-Route::get('/dashboard', function () {
+Route::get('dashboard', function () {
     if (Auth::user()->hasRole('Admin')) {
         return Inertia::location(route('admin.dashboard', absolute: false));
     } else if (Auth::user()->hasRole('Teacher')) {
@@ -28,10 +29,10 @@ Route::get('/dashboard', function () {
 
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::controller(PaymentController::class)->group(function () {
+    Route::post('payments', 'create')->name('payments.create');
+    Route::post('payments/callback', 'callback')->name('payments.callback');
+    Route::get('payments/success', 'success')->name('payments.success');
 });
 
 require __DIR__.'/auth.php';
