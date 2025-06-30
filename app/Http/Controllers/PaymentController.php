db25c5d23f74523e83f93bd1e2716216de8a65d5
RinnHehe <wafflemelons@gmail.com>
2025-07-01 02:35:12 +0800

    Midtrans - Callback Midtrans

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\FeeStatus;
 use App\Models\Fee;
 use Exception;
 use Illuminate\Http\JsonResponse;
@@ -52,6 +53,80 @@ class PaymentController extends Controller
             return response()->json([
                 'error' => $e->getMessage(),
             ], 500);
+        }
+    }
+
+    public function callback(Request $request): JsonResponse
+    {
+        $serverKey = config('services.midtrans.server_key');
+        $signatureKey = signatureMidtrans(
+            $request->fee_code,
+            $request->status_code,
+            $request->gross_amount,
+            $serverKey
+        );
+
+        if ($request->signature_key != $signatureKey) {
+            return response()->json([
+                'error' => 'Unauthorized',
+            ], 401);
+        }
+
+        $fee = Fee::query()
+        ->where('fee_code', $request->fee_code)
+        ->first();
+
+        if(!$fee) {
+            return response()->json([
+                'message' => 'Pembayaran tidak ditemukan',
+            ], 404);
+        }
+
+        switch ($request->transaction_status) {
+            case 'settlement':
+                $fee->status = FeeStatus::SUCCESS->value;
+                $fee->save();
+
+                return response()->json([
+                    'message' => 'Pembayaran melakukan pembayaran',
+                ], 200);
+                break;
+            case 'capture':
+                $fee->status = FeeStatus::SUCCESS->value;
+                $fee->save();
+
+                return response()->json([
+                    'message' => 'Pembayaran melakukan pembayaran',
+                ], 200);
+                break;
+            case 'pending':
+                $fee->status = FeeStatus::PENDING->value;
+                $fee->save();
+
+                return response()->json([
+                    'message' => 'Pembayaran tertunda',
+                ], 200);
+                break;
+            case 'expire':
+                $fee->status = FeeStatus::FAILED->value;
+                $fee->save();
+
+                return response()->json([
+                    'message' => 'Pembayaran kadaluarsa',
+                ], 200);
+                break;
+            case 'cancel':
+                $fee->status = FeeStatus::FAILED->value;
+                $fee->save();
+
+                return response()->json([
+                    'message' => 'Pembayaran dibatalkan',
+                ], 200);
+                break;
+            default:
+                return response()->json([
+                    'message' => 'Status transaksi tidak diketahui',
+                ], 400);
         }
     }
 }
