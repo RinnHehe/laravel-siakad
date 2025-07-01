f8c632daee63c8ecb5cd01a15cd6f68f761acac2
RinnHehe <wafflemelons@gmail.com>
2025-07-01 21:06:29 +0800

    Middleware - Check Active Academic

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\CheckActiveAcademicYear;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -19,6 +20,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'payments/callback',
         ])->alias(aliases:[
             'role' => RoleMiddleware::class,
+            'checkActiveAcademicYear' => CheckActiveAcademicYear::class,
         ]);
 
         //
